786162a27fd3d90e69e2d4f2f9fc27277de6c54e
CrazyGamerGR <frankalexb97@gmail.com>
2017-08-20 23:27:17 +0300

    update the versioning code

--- init/version.c
+++ init/version.c
@@ -41,10 +41,10 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " (CrazySuperKernel)"
+	"Linux version " UTS_RELEASE " (CrazyAquaKernel)"
 	"(" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (CrazySuperKernel)"
+	" (CrazyAquaKernel)"
 	" (" LINUX_COMPILER ") %s\n";
