077a6c9b4b530fbfa187c00a7f886d0d0da449e0
CrazyGamerGR <frankalexb97@gmail.com>
2017-08-20 23:26:20 +0300

    update build_anykernel.sh

--- build_anykernel.sh
+++ build_anykernel.sh
@@ -8,16 +8,16 @@
 #   		\_______|    |__|\___|
 ##########################################
 export ARCH=arm64
-export CROSS_COMPILE=/home/francesco/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/francesco/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 ##########################################
 mkdir outputh830
 mkdir outputh850
 ##########################################
 make -C $(pwd) O=outputh830 cyanogenmod_h830_defconfig
-make -C $(pwd) O=outputh830
+make -j64 -C $(pwd) O=outputh830
 
 make -C $(pwd) O=outputh850 cyanogenmod_h850_defconfig
-make -C $(pwd) O=outputh850
+make -j64 -C $(pwd) O=outputh850
 ##########################################
 cp outputh830/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
 cp outputh850/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
