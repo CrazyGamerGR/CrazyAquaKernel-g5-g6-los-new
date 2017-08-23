3fa13bda68bd187ca58a11b0dbd523f1f4910176
Alex Francesco Branco <frankalexb1997@gmail.com>
2017-08-23 23:35:24 +0300

    Update build_anykernel.sh

--- build_anykernel.sh
+++ build_anykernel.sh
@@ -9,15 +9,30 @@
 ##########################################
 export ARCH=arm64
 export CROSS_COMPILE=/home/francesco/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-##########################################
+
+##############G5############################
 mkdir outputh830
 mkdir outputh850
-##########################################
-make -C $(pwd) O=outputh830 cyanogenmod_h830_defconfig
+##############G6############################
+mkdir outputh870
+mkdir outputus997
+
+##############G5############################
+make -C $(pwd) O=outputh830 lineageos_h830_defconfig
 make -j64 -C $(pwd) O=outputh830
 
-make -C $(pwd) O=outputh850 cyanogenmod_h850_defconfig
+make -C $(pwd) O=outputh850 lineageos_h850_defconfig
 make -j64 -C $(pwd) O=outputh850
-##########################################
+##############G6############################
+make -C $(pwd) O=outputh870 lineageos_h870_defconfig
+make -j64 -C $(pwd) O=outputh870
+
+make -C $(pwd) O=outputus997 lineageos_us997_defconfig
+make -j64 -C $(pwd) O=outputus997
+
+##############G5############################
 cp outputh830/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
 cp outputh850/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
+##############G6############################
+cp outputh870/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
+cp outputus997/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
